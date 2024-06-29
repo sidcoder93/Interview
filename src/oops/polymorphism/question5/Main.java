b8611298823b7ca426c5bdf1fc7c2e1193b17214
siddharth <vaiddyasiddharth17@gmail.com>
2024-06-29 10:07:16 +0530

    29-JUNE-24: new code added

--- src/oops/polymorphism/question5/Main.java
+++ src/oops/polymorphism/question5/Main.java
@@ -1,10 +1,46 @@
 package oops.polymorphism.question5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-        {
-            new Y().method(100);
+
+        Object[] array = { 1, 2, new Object[]{ 3, 4, new Object[]{ 5 }, 6, 7 }, 8, 9, 10 };
+
+
+
+        List<Object> flattenedList = flatten(array);
+
+        Object[] flattenedArray = flattenedList.toArray();
+        for (Object obj : flattenedArray) {
+            System.out.print(obj + " ");
         }
+
+
     }
+
+    public static List<Object> flatten(Object[] array) {
+        List<Object> resultList = new ArrayList<>();
+        flatten(array, resultList);
+        return resultList;
+    }
+
+    private static void flatten(Object[] array, List<Object> resultList) {
+        for (Object element : array) {
+            if (element instanceof Object[]) {
+                // Recursively flatten the nested array
+                flatten((Object[]) element, resultList);
+            } else {
+                // Add non-array elements directly to the result list
+                resultList.add(element);
+            }
+        }
+
+
+    }
+
 }
